2ecfdad6a43a4329abca25472781174a6745630d
Evgeniy Blinov <zheni-b@yandex.ru>
2024-08-13 13:29:34 +0300

    autosplit of commands by default

--- tests/test_proxy_module.py
+++ tests/test_proxy_module.py
@@ -14,43 +14,79 @@ import suby
 from suby import RunningCommandError
 
 
-def test_normal_way():
-    result = suby(sys.executable, '-c', 'print("kek")')
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "print(\'kek\')"'),),
+        (('python -c "print(\'kek\')"',),),
+    ]
+)
+def test_normal_way(command):
+    result = suby(*command)
 
     assert result.stdout == 'kek\n'
     assert result.stderr == ''
     assert result.returncode == 0
 
 
-def test_normal_way_with_simple_token():
-    result = suby(sys.executable, '-c', 'print("kek")', token=SimpleToken())
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "print(\'kek\')"'),),
+        (('python -c "print(\'kek\')"',),),
+    ]
+)
+def test_normal_way_with_simple_token(command):
+    result = suby(*command, token=SimpleToken())
 
     assert result.stdout == 'kek\n'
     assert result.stderr == ''
     assert result.returncode == 0
 
 
-def test_stderr_catching():
-    result = suby(sys.executable, '-c', 'import sys; sys.stderr.write("kek")')
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "import sys; sys.stderr.write(\'kek\')"'),),
+        (('python -c "import sys; sys.stderr.write(\'kek\')"',),),
+    ]
+)
+def test_stderr_catching(command):
+    result = suby(*command)
 
     assert result.stdout == ''
     assert result.stderr == 'kek'
     assert result.returncode == 0
 
 
-def test_catch_exception():
-    result = suby(sys.executable, '-c', 'raise ValueError', catch_exceptions=True)
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "raise ValueError"'),),
+        (('python -c "raise ValueError"',),),
+    ]
+)
+def test_catch_exception(command):
+    result = suby(*command, catch_exceptions=True)
 
     assert 'ValueError' in result.stderr
     assert result.returncode != 0
 
 
-def test_timeout():
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "import time; time.sleep({sleep_time})"'),),
+        (('python -c "import time; time.sleep({sleep_time})"',),),
+    ]
+)
+def test_timeout(command):
     sleep_time = 100000
     timeout = 0.001
+    command = [subcommand.format(sleep_time=sleep_time) if isinstance(subcommand, str) else subcommand for subcommand in command]
 
     start_time = perf_counter()
-    result = suby(sys.executable, '-c', f'import time; time.sleep({sleep_time})', timeout=timeout, catch_exceptions=True)
+    result = suby(*command, timeout=timeout, catch_exceptions=True)
     end_time = perf_counter()
 
     assert result.returncode != 0
@@ -61,16 +97,24 @@ def test_timeout():
     assert (end_time - start_time) >= timeout
 
 
-def test_timeout_without_catching_exception():
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "import time; time.sleep({sleep_time})"'),),
+        (('python -c "import time; time.sleep({sleep_time})"',),),
+    ]
+)
+def test_timeout_without_catching_exception(command):
     sleep_time = 100000
     timeout = 0.001
+    command = [subcommand.format(sleep_time=sleep_time) if isinstance(subcommand, str) else subcommand for subcommand in command]
 
     with pytest.raises(TimeoutCancellationError):
-        suby(sys.executable, '-c', f'import time; time.sleep({sleep_time})', timeout=timeout)
+        suby(*command, timeout=timeout)
 
     start_time = perf_counter()
     try:
-        suby(sys.executable, '-c', f'import time; time.sleep({sleep_time})', timeout=timeout)
+        suby(*command, timeout=timeout)
     except TimeoutCancellationError as e:
         assert e.result.stdout == ''
         assert e.result.stderr == ''
@@ -81,24 +125,38 @@ def test_timeout_without_catching_exception():
     assert (end_time - start_time) >= timeout
 
 
-def test_exception_in_subprocess_without_catching():
-    with pytest.raises(RunningCommandError, match=re.escape(f'Error when executing the command "{sys.executable} -c "raise ValueError"".')):
-        suby(sys.executable, '-c', 'raise ValueError')
+@pytest.mark.parametrize(
+    ['command', 'error_text'],
+    [
+        ((sys.executable, '-c "raise ValueError"'), f'Error when executing the command "{sys.executable} -c "raise ValueError"".'),
+        (('python -c "raise ValueError"',), 'Error when executing the command "python -c "raise ValueError"".'),
+    ]
+)
+def test_exception_in_subprocess_without_catching(command, error_text):
+    with pytest.raises(RunningCommandError, match=re.escape(error_text)):
+        suby(*command)
 
     try:
-        suby(sys.executable, '-c', 'raise ValueError')
+        suby(*command)
     except RunningCommandError as e:
         assert e.result.stdout == ''
         assert 'ValueError' in e.result.stderr
         assert e.result.returncode != 0
 
 
-def test_not_catching_output():
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "print(\'kek1\', end=\'\'); import sys; sys.stderr.write(\'kek2\')"'),),
+        (('python -c "print(\'kek1\', end=\'\'); import sys; sys.stderr.write(\'kek2\')"',),),
+    ]
+)
+def test_not_catching_output(command):
     stderr_buffer = StringIO()
     stdout_buffer = StringIO()
 
     with redirect_stdout(stdout_buffer), redirect_stderr(stderr_buffer):
-        result = suby(sys.executable, '-c', 'print("kek1", end=""); import sys; sys.stderr.write("kek2")', catch_output=False)
+        result = suby(*command, catch_output=False)
 
         stderr = stderr_buffer.getvalue()
         stdout = stdout_buffer.getvalue()
@@ -108,88 +166,142 @@ def test_not_catching_output():
         assert stdout == 'kek1'
 
 
-def test_catching_output():
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "print(\'kek1\', end=\'\'); import sys; sys.stderr.write(\'kek2\')"'),),
+        (('python -c "print(\'kek1\', end=\'\'); import sys; sys.stderr.write(\'kek2\')"',),),
+    ]
+)
+def test_catching_output(command):
     stderr_buffer = StringIO()
     stdout_buffer = StringIO()
 
     with redirect_stdout(stdout_buffer), redirect_stderr(stderr_buffer):
-        result = suby(sys.executable, '-c', 'print("kek1", end=""); import sys; sys.stderr.write("kek2")', catch_output=True)
+        result = suby(*command, catch_output=True)
 
         assert result.returncode == 0
         assert stderr_buffer.getvalue() == ''
         assert stdout_buffer.getvalue() == ''
 
 
-def test_logging_normal_way():
+@pytest.mark.parametrize(
+    ['command', 'first_log_message', 'second_log_message'],
+    [
+        ((sys.executable, '-c "print(\'kek\', end=\'\')"'), f'The beginning of the execution of the command "{sys.executable} -c "print(\'kek\', end=\'\')"".', f'The command "{sys.executable} -c "print(\'kek\', end=\'\')"" has been successfully executed.'),
+        (('python -c "print(\'kek\', end=\'\')"',), 'The beginning of the execution of the command "python -c "print(\'kek\', end=\'\')"".', 'The command "python -c "print(\'kek\', end=\'\')"" has been successfully executed.'),
+    ]
+)
+def test_logging_normal_way(command, first_log_message, second_log_message):
     logger = MemoryLogger()
 
-    suby(sys.executable, '-c', 'print("kek", end="")', logger=logger, catch_output=True)
+    suby(*command, logger=logger, catch_output=True)
 
     assert len(logger.data.info) == 2
     assert len(logger.data.error) == 0
+    assert len(logger.data) == 2
 
-    assert logger.data.info[0].message == f'The beginning of the execution of the command "{sys.executable} -c "print("kek", end="")"".'
-    assert logger.data.info[1].message == f'The command "{sys.executable} -c "print("kek", end="")"" has been successfully executed.'
+    assert logger.data.info[0].message == first_log_message
+    assert logger.data.info[1].message == second_log_message
 
 
-def test_logging_with_expired_timeout():
+@pytest.mark.parametrize(
+    ['command', 'first_log_message', 'second_log_message'],
+    [
+        ((sys.executable, f'-c "import time; time.sleep({500_000})"'), f'The beginning of the execution of the command "{sys.executable} -c "import time; time.sleep(500000)"".', f'The execution of the "{sys.executable} -c "import time; time.sleep(500000)"" command was canceled using a cancellation token.'),
+        ((f'python -c "import time; time.sleep({500_000})"',), 'The beginning of the execution of the command "python -c "import time; time.sleep(500000)"".', 'The execution of the "python -c "import time; time.sleep(500000)"" command was canceled using a cancellation token.'),
+    ]
+)
+def test_logging_with_expired_timeout(command, first_log_message, second_log_message):
     logger = MemoryLogger()
 
-    suby(sys.executable, '-c', f'import time; time.sleep({500_000})', logger=logger, catch_exceptions=True, catch_output=True, timeout=0.0001)
+    suby(*command, logger=logger, catch_exceptions=True, catch_output=True, timeout=0.0001)
 
     assert len(logger.data.info) == 1
     assert len(logger.data.error) == 1
+    assert len(logger.data) == 2
 
-    assert logger.data.info[0].message == f'The beginning of the execution of the command "{sys.executable} -c "import time; time.sleep(500000)"".'
-    assert logger.data.error[0].message == f'The execution of the "{sys.executable} -c "import time; time.sleep(500000)"" command was canceled using a cancellation token.'
+    assert logger.data.info[0].message == first_log_message
+    assert logger.data.error[0].message == second_log_message
 
 
-def test_logging_with_exception():
+@pytest.mark.parametrize(
+    ['command', 'first_log_message', 'second_log_message'],
+    [
+        ((sys.executable, '-c 1/0'), f'The beginning of the execution of the command "{sys.executable} -c 1/0".', f'Error when executing the command "{sys.executable} -c 1/0".'),
+        (('python -c 1/0',), 'The beginning of the execution of the command "python -c 1/0".', 'Error when executing the command "python -c 1/0".'),
+    ]
+)
+def test_logging_with_exception(command, first_log_message, second_log_message):
     logger = MemoryLogger()
 
-    suby(sys.executable, '-c', '1/0', logger=logger, catch_exceptions=True, catch_output=True)
+    suby(*command, logger=logger, catch_exceptions=True, catch_output=True)
 
     assert len(logger.data.info) == 1
     assert len(logger.data.error) == 1
+    assert len(logger.data) == 2
 
-    assert logger.data.info[0].message == f'The beginning of the execution of the command "{sys.executable} -c 1/0".'
-    assert logger.data.error[0].message == f'Error when executing the command "{sys.executable} -c 1/0".'
+    assert logger.data.info[0].message == first_log_message
+    assert logger.data.error[0].message == second_log_message
 
 
-def test_logging_with_expired_timeout_without_catching_exceptions():
+@pytest.mark.parametrize(
+    ['command', 'first_log_message', 'second_log_message'],
+    [
+        ((sys.executable, f'-c "import time; time.sleep({500_000})"'), f'The beginning of the execution of the command "{sys.executable} -c "import time; time.sleep(500000)"".', f'The execution of the "{sys.executable} -c "import time; time.sleep(500000)"" command was canceled using a cancellation token.'),
+        ((f'python -c "import time; time.sleep({500_000})"',), 'The beginning of the execution of the command "python -c "import time; time.sleep(500000)"".', 'The execution of the "python -c "import time; time.sleep(500000)"" command was canceled using a cancellation token.'),
+    ]
+)
+def test_logging_with_expired_timeout_without_catching_exceptions(command, first_log_message, second_log_message):
     logger = MemoryLogger()
 
     with pytest.raises(TimeoutCancellationError):
-        suby(sys.executable, '-c', f'import time; time.sleep({500_000})', logger=logger, catch_output=True, timeout=0.0001)
+        suby(*command, logger=logger, catch_output=True, timeout=0.0001)
 
     assert len(logger.data.info) == 1
     assert len(logger.data.error) == 1
+    assert len(logger.data) == 2
 
-    assert logger.data.info[0].message == f'The beginning of the execution of the command "{sys.executable} -c "import time; time.sleep(500000)"".'
-    assert logger.data.error[0].message == f'The execution of the "{sys.executable} -c "import time; time.sleep(500000)"" command was canceled using a cancellation token.'
+    assert logger.data.info[0].message == first_log_message
+    assert logger.data.error[0].message == second_log_message
 
 
-def test_logging_with_exception_without_catching_exceptions():
+@pytest.mark.parametrize(
+    ['command', 'first_log_message', 'second_log_message'],
+    [
+        ((sys.executable, f'-c 1/0'), f'The beginning of the execution of the command "{sys.executable} -c 1/0".', f'Error when executing the command "{sys.executable} -c 1/0".'),
+        ((f'python -c 1/0',), f'The beginning of the execution of the command "python -c 1/0".', f'Error when executing the command "python -c 1/0".'),
+    ]
+)
+def test_logging_with_exception_without_catching_exceptions(command, first_log_message, second_log_message):
     logger = MemoryLogger()
 
     with pytest.raises(RunningCommandError):
-        suby(sys.executable, '-c', '1/0', logger=logger, catch_output=True)
+        suby(*command, logger=logger, catch_output=True)
 
     assert len(logger.data.info) == 1
     assert len(logger.data.error) == 1
 
-    assert logger.data.info[0].message == f'The beginning of the execution of the command "{sys.executable} -c 1/0".'
-    assert logger.data.error[0].message == f'Error when executing the command "{sys.executable} -c 1/0".'
+    assert logger.data.info[0].message == first_log_message
+    assert logger.data.error[0].message == second_log_message
 
 
-def test_only_token():
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "import time; time.sleep({sleep_time})"'),),
+        (('python -c "import time; time.sleep({sleep_time})"',),),
+    ]
+)
+def test_only_token(command):
     sleep_time = 100000
     timeout = 0.1
+    command = [subcommand.format(sleep_time=sleep_time) if isinstance(subcommand, str) else subcommand for subcommand in command]
 
     start_time = perf_counter()
     token = ConditionToken(lambda: perf_counter() - start_time > timeout)
 
-    result = suby(sys.executable, '-c', f'import time; time.sleep({sleep_time})', catch_exceptions=True, token=token)
+    result = suby(*command, catch_exceptions=True, token=token)
 
     end_time = perf_counter()
 
@@ -202,15 +314,23 @@ def test_only_token():
     assert end_time - start_time < sleep_time
 
 
-def test_only_token_without_catching():
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "import time; time.sleep({sleep_time})"'),),
+        (('python -c "import time; time.sleep({sleep_time})"',),),
+    ]
+)
+def test_only_token_without_catching(command):
     sleep_time = 100000
     timeout = 0.1
+    command = [subcommand.format(sleep_time=sleep_time) if isinstance(subcommand, str) else subcommand for subcommand in command]
 
     start_time = perf_counter()
     token = ConditionToken(lambda: perf_counter() - start_time > timeout)
 
     try:
-        result = suby(sys.executable, '-c', f'import time; time.sleep({sleep_time})', token=token)
+        result = suby(*command, token=token)
     except ConditionCancellationError as e:
         assert e.token is token
         result = e.result
@@ -226,15 +346,23 @@ def test_only_token_without_catching():
     assert end_time - start_time < sleep_time
 
 
-def test_token_plus_timeout_but_timeout_is_more_without_catching():
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "import time; time.sleep({sleep_time})"'),),
+        (('python -c "import time; time.sleep({sleep_time})"',),),
+    ]
+)
+def test_token_plus_timeout_but_timeout_is_more_without_catching(command):
     sleep_time = 100000
     timeout = 0.1
+    command = [subcommand.format(sleep_time=sleep_time) if isinstance(subcommand, str) else subcommand for subcommand in command]
 
     start_time = perf_counter()
     token = ConditionToken(lambda: perf_counter() - start_time > timeout)
 
     try:
-        result = suby(sys.executable, '-c', f'import time; time.sleep({sleep_time})', token=token, timeout=3)
+        result = suby(*command, token=token, timeout=3)
     except ConditionCancellationError as e:
         assert e.token is token
         result = e.result
@@ -250,15 +378,23 @@ def test_token_plus_timeout_but_timeout_is_more_without_catching():
     assert end_time - start_time < sleep_time
 
 
-def test_token_plus_timeout_but_timeout_is_less_without_catching():
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "import time; time.sleep({sleep_time})"'),),
+        (('python -c "import time; time.sleep({sleep_time})"',),),
+    ]
+)
+def test_token_plus_timeout_but_timeout_is_less_without_catching(command):
     sleep_time = 100000
     timeout = 0.1
+    command = [subcommand.format(sleep_time=sleep_time) if isinstance(subcommand, str) else subcommand for subcommand in command]
 
     start_time = perf_counter()
     token = ConditionToken(lambda: perf_counter() - start_time > timeout)
 
     try:
-        result = suby(sys.executable, '-c', f'import time; time.sleep({sleep_time})', token=token, timeout=timeout/2)
+        result = suby(*command, token=token, timeout=timeout/2)
     except TimeoutCancellationError as e:
         assert e.token is not token
         result = e.result
@@ -274,14 +410,21 @@ def test_token_plus_timeout_but_timeout_is_less_without_catching():
     assert end_time - start_time < sleep_time
 
 
-def test_replace_stdout_callback():
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "print(\'kek\')"'),),
+        (('python -c "print(\'kek\')"',),),
+    ]
+)
+def test_replace_stdout_callback(command):
     accumulator = []
 
     stderr_buffer = StringIO()
     stdout_buffer = StringIO()
 
     with redirect_stdout(stdout_buffer), redirect_stderr(stderr_buffer):
-        result = suby(sys.executable, '-c', 'print("kek")', stdout_callback=lambda x: accumulator.append(x))
+        result = suby(*command, stdout_callback=lambda x: accumulator.append(x))
 
     assert accumulator == ['kek\n']
 
@@ -293,14 +436,21 @@ def test_replace_stdout_callback():
     assert stdout_buffer.getvalue() == ''
 
 
-def test_replace_stderr_callback():
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((sys.executable, '-c "import sys; sys.stderr.write(\'kek\')"'),),
+        (('python -c "import sys; sys.stderr.write(\'kek\')"',),),
+    ]
+)
+def test_replace_stderr_callback(command):
     accumulator = []
 
     stderr_buffer = StringIO()
     stdout_buffer = StringIO()
 
     with redirect_stdout(stdout_buffer), redirect_stderr(stderr_buffer):
-        result = suby(sys.executable, '-c', 'import sys; sys.stderr.write("kek")', stderr_callback=lambda x: accumulator.append(x))
+        result = suby(*command, stderr_callback=lambda x: accumulator.append(x))
 
     assert accumulator == ['kek']
 
@@ -325,8 +475,16 @@ def test_pass_wrong_positional_argument(arguments, exception_message):
         suby(*arguments)
 
 
-def test_use_path_object_as_first_positional_argument():
-    result = suby(Path(sys.executable), '-c', 'print("kek")')
+@pytest.mark.parametrize(
+    ['command'],
+    [
+        ((Path(sys.executable), '-c "print(\'kek\')"'),),
+        ((sys.executable, '-c "print(\'kek\')"'),),
+        (('python -c "print(\'kek\')"',),),
+    ]
+)
+def test_use_path_object_as_first_positional_argument(command):
+    result = suby(*command)
 
     assert result.stdout == 'kek\n'
     assert result.stderr == ''
