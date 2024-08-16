f7e3d5b567362534b1762156a4dc678d2015000f
Evgeniy Blinov <zheni-b@yandex.ru>
2024-08-16 18:28:48 +0300

    different tests for windows and not windows

--- tests/documentation/test_quick_start.py
+++ tests/documentation/test_quick_start.py
@@ -1,15 +1,35 @@
 from io import StringIO
+import platform
 from contextlib import redirect_stdout, redirect_stderr
 
 import suby
+import pytest
 
 
-def test_run_hello_world():
+@pytest.mark.skipif(platform.system() == 'Windows', reason='Windows and not windows have different rules of escaping characters.')
+def test_run_hello_world_not_windows():
     stderr_buffer = StringIO()
     stdout_buffer = StringIO()
 
     with redirect_stdout(stdout_buffer), redirect_stderr(stderr_buffer):
         result = suby('python -c "print(\'hello, world!\')"')
+
+    assert stderr_buffer.getvalue() == ''
+    assert stdout_buffer.getvalue() == 'hello, world!\n'
+
+    assert result.stdout == 'hello, world!\n'
+    assert result.stderr == ''
+    assert result.returncode == 0
+    assert not result.killed_by_token
+
+
+@pytest.mark.skipif(platform.system() != 'Windows', reason='Windows and not windows have different rules of escaping characters.')
+def test_run_hello_world_windows():
+    stderr_buffer = StringIO()
+    stdout_buffer = StringIO()
+
+    with redirect_stdout(stdout_buffer), redirect_stderr(stderr_buffer):
+        result = suby('python -c "print^(\'hello,world^!\'^)"')
 
     assert stderr_buffer.getvalue() == ''
     assert stdout_buffer.getvalue() == 'hello, world!\n'
